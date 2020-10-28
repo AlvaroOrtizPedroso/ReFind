e2165ea36792be9f0d6adddfa938c3197fddefd2
AlvaroOrtiz <alorpes@gmail.com>
2020-10-28 20:15:45 +0100

    Funcionalidad de acceder a al galeria

--- app/src/main/java/com/example/refind/Perfil.java
+++ app/src/main/java/com/example/refind/Perfil.java
@@ -1,13 +1,83 @@
 package com.example.refind;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.Toast;
 
 public class Perfil extends AppCompatActivity {
+
+    ImageButton fotoUsuario;
+
+    private static final int REQUEST_PERMISSION_CODE = 100;
+    private static final int REQUEST_IMAGE_GALLERY = 101;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
+
+        fotoUsuario = findViewById(R.id.fotoUsuario);
+
+        fotoUsuario.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
+                    if(ActivityCompat.checkSelfPermission(Perfil.this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED){
+                        openGallery();
+                    }else{
+                        ActivityCompat.requestPermissions(Perfil.this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},REQUEST_PERMISSION_CODE);
+                    }
+                }else{
+                    openGallery();
+                }
+            }
+        });
+    }
+
+    // METODOS PARA OBTENER LOS PERMISOS Y ADQUIRIR UNA FOTO DE LA GALERIA
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if(requestCode == REQUEST_IMAGE_GALLERY){
+            if(resultCode == Activity.RESULT_OK && data!=null){
+                Uri photo = data.getData();
+                // nombre que hace referencia a la ImagenViewq
+                fotoUsuario.setImageURI(photo);
+            }else{
+                Log.i("TAG", "Resultado: "+ resultCode);
+                Toast.makeText(this, "No se escogio imagen de la galeria", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if(requestCode == REQUEST_PERMISSION_CODE){
+            if(permissions.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                openGallery();
+            }else{
+                Toast.makeText(this, "Necesitas activar los permisos", Toast.LENGTH_SHORT).show();
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void openGallery(){
+        Intent intent = new Intent(Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, REQUEST_IMAGE_GALLERY);
     }
 }

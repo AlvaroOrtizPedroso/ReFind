bd2cb42a72d513eb496d6c0503f6fbbd3fb124f2
AlvaroOrtiz <alorpes@gmail.com>
2020-11-17 00:07:55 +0100

    Creaccion de los intent de Login y registro con comentarios

--- app/src/main/java/com/example/refind/Login.java
+++ app/src/main/java/com/example/refind/Login.java
@@ -2,10 +2,16 @@ package com.example.refind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.Toast;
 
 public class Login extends AppCompatActivity {
+
+    //Creamos los dos objetos de tipo EditText para el correo y la contraseña
+    private EditText correo, password;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +19,34 @@ public class Login extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         Toast.makeText(this, "Aplicacion iniciada: Login", Toast.LENGTH_LONG).show();
         this.setTitle("Iniciar sesión");
+
+        //Relacionamos los dos obtejos EditText con el id correspondiente
+        correo = (EditText)findViewById(R.id.emailLogin);
+        password = (EditText)findViewById(R.id.passLogin);
+    }
+
+    /**
+     * Metodo asociado al boton iniciarSesion para mandar a la activity MainActivity
+     * Falta la comprobacion de que los datos sean correctos
+     * @param view
+     */
+    public void iniciarSesion(View view){
+        if(true){
+            Intent login_MainActivity = new Intent(this, MainActivity.class);
+            startActivity(login_MainActivity);
+        }
+        else{
+            Toast.makeText(this, "Datos introducidos erroneos", Toast.LENGTH_LONG).show();
+        }
+    }
+
+    /**
+     * Metodo asociado al boton Registro el cual mandara a la activity Registro
+     * @param view
+     */
+    public void toRegistro(View view){
+            Intent login_registro = new Intent(this, Registro.class);
+            startActivity(login_registro);
     }
 }
 
--- app/src/main/java/com/example/refind/Registro.java
+++ app/src/main/java/com/example/refind/Registro.java
@@ -2,9 +2,11 @@ package com.example.refind;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
 public class Registro extends AppCompatActivity {
@@ -15,26 +17,56 @@ public class Registro extends AppCompatActivity {
         setContentView(R.layout.activity_registro);
         Toast.makeText(this, "Aplicacion iniciada: Registro", Toast.LENGTH_LONG).show();
     }
-    // METODOS PARA EL MENU DESPLEGABLE
+
+    /**
+     * Metodo asociado al boton iniciarSesion para mandar a la activity login
+     * @param view
+     */
+    public void iniciarSesion(View view){
+        Intent registro_login = new Intent(this, Login.class);
+        startActivity(registro_login);
+    }
+
+    /**
+     * Metodo asociado al boton registro
+     *  //Falta por hacer
+     *      -comprobar los datos que sean correcto
+     *      -Insertar los datos en la base de datos
+     *      -Logear directamente
+     * @param view
+     */
+    public void registrar(View view){
+        if(true){
+            Intent registro_MainActivity = new Intent(this, MainActivity.class);
+            startActivity(registro_MainActivity);
+        }
+        else{
+            Toast.makeText(this, "Datos introducidos erroneos", Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
+
 
     /**
      * Metoto para mostrar u ocultar el menu
      * Debemos crear un metodo con este nombre exactamente al cual le pasamos un parametro de tipo MenuItem
      * @param menu
      * @return
-     */
+
     public boolean onCreateOptionsMenu(Menu menu){
         //Carpeta res - menu - overflow y pasamos el objeto menu
         getMenuInflater().inflate(R.menu.overflow, menu);
         return true;
     }
 
-    /**
+
      * Metodo para indicar a cual en cual de las opciones se hizo clic
      * Debemos crear un metodo con este nombre exactamente al cual le pasamos un parametro de tipo MenuItem
      * @param item
      * @return
-     */
+
     public boolean onOptionsItemSelected(MenuItem item){
         int id= item.getItemId();
 
@@ -48,5 +80,5 @@ public class Registro extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
